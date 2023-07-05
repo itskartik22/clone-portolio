7b843de2531f918c06704f02b7de1c8d1ed993e9
Kartik Thakur <thakurkartik2262@gmail.com>
2023-07-06 01:21:33 +0530

    nav-bar-responsive

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+// Nav Menu Buttom
+const openBtn = document.querySelector(".nav-menu-open");
+const closeBtn = document.querySelector(".nav-menu-close");
+const navList = document.querySelector(".nav-list");
+
+function navListToggle(){
+  navList.classList.toggle("menu-open");
+}
+
+openBtn.addEventListener('click', navListToggle);
+closeBtn.addEventListener('click', navListToggle);
+
 // Typing Animation
 const skillsArray = [
   "Web Developement",
@@ -146,6 +158,8 @@ function handleClick(event){
          })
 
     }
+    // Close Side-menu
+    navList.classList.toggle("menu-open");
 }
 
 navLinks.forEach(link => link.addEventListener('click', handleClick))
@@ -156,81 +170,81 @@ navLinks.forEach(link => link.addEventListener('click', handleClick))
 // var container = document.getElementsByClassName("img-container");
 // var inner = document.getElementsByClassName("img-inner");
 
-(function() {
-    // Init
-    var container = document.getElementsByClassName("img-container"),
-      inner = document.getElementsByClassName("img-inner");
+// (function() {
+//     // Init
+//     var container = document.getElementsByClassName("img-container"),
+//       inner = document.getElementsByClassName("img-inner");
   
-    // Mouse
-    var mouse = {
-      _x: 0,
-      _y: 0,
-      x: 0,
-      y: 0,
-      updatePosition: function(event) {
-        var e = event || window.event;
-        this.x = e.clientX - this._x;
-        this.y = (e.clientY - this._y) * -1;
-      },
-      setOrigin: function(e) {
-        this._x = e.offsetLeft + Math.floor(e.offsetWidth / 2);
-        this._y = e.offsetTop + Math.floor(e.offsetHeight / 2);
-      },
-      show: function() {
-        return "(" + this.x + ", " + this.y + ")";
-      }
-    };
+//     // Mouse
+//     var mouse = {
+//       _x: 0,
+//       _y: 0,
+//       x: 0,
+//       y: 0,
+//       updatePosition: function(event) {
+//         var e = event || window.event;
+//         this.x = e.clientX - this._x;
+//         this.y = (e.clientY - this._y) * -1;
+//       },
+//       setOrigin: function(e) {
+//         this._x = e.offsetLeft + Math.floor(e.offsetWidth / 2);
+//         this._y = e.offsetTop + Math.floor(e.offsetHeight / 2);
+//       },
+//       show: function() {
+//         return "(" + this.x + ", " + this.y + ")";
+//       }
+//     };
   
-    // Track the mouse position relative to the center of the container.
-    mouse.setOrigin(container);
+//     // Track the mouse position relative to the center of the container.
+//     mouse.setOrigin(container);
   
-    //-----------------------------------------
+//     //-----------------------------------------
   
-    var counter = 0;
-    var updateRate = 10;
-    var isTimeToUpdate = function() {
-      return counter++ % updateRate === 0;
-    };
+//     var counter = 0;
+//     var updateRate = 10;
+//     var isTimeToUpdate = function() {
+//       return counter++ % updateRate === 0;
+//     };
   
-    //-----------------------------------------
+//     //-----------------------------------------
   
-    var onMouseEnterHandler = function(event) {
-      update(event);
-    };
+//     var onMouseEnterHandler = function(event) {
+//       update(event);
+//     };
   
-    var onMouseLeaveHandler = function() {
-      inner.style = "";
-    };
+//     var onMouseLeaveHandler = function() {
+//       inner.style = "";
+//     };
   
-    var onMouseMoveHandler = function(event) {
-      if (isTimeToUpdate()) {
-        update(event);
-      }
-    };
+//     var onMouseMoveHandler = function(event) {
+//       if (isTimeToUpdate()) {
+//         update(event);
+//       }
+//     };
   
-    //-----------------------------------------
+//     //-----------------------------------------
   
-    var update = function(event) {
-      mouse.updatePosition(event);
-      updateTransformStyle(
-        (mouse.y / inner.offsetHeight / 2).toFixed(2),
-        (mouse.x / inner.offsetWidth / 2).toFixed(2)
-      );
-    };
+//     var update = function(event) {
+//       mouse.updatePosition(event);
+//       updateTransformStyle(
+//         (mouse.y / inner.offsetHeight / 2).toFixed(2),
+//         (mouse.x / inner.offsetWidth / 2).toFixed(2)
+//       );
+//     };
   
-    var updateTransformStyle = function(x, y) {
-      var style = "rotateX(" + x + "deg) rotateY(" + y + "deg)";
-      inner.style.transform = style;
-      inner.style.webkitTransform = style;
-      inner.style.mozTransform = style;
-      inner.style.msTransform = style;
-      inner.style.oTransform = style;
-    };
+//     var updateTransformStyle = function(x, y) {
+//       var style = "rotateX(" + x + "deg) rotateY(" + y + "deg)";
+//       inner.style.transform = style;
+//       inner.style.webkitTransform = style;
+//       inner.style.mozTransform = style;
+//       inner.style.msTransform = style;
+//       inner.style.oTransform = style;
+//     };
   
-    //-----------------------------------------
+//     //-----------------------------------------
   
-    container.onmouseenter = onMouseEnterHandler;
-    container.onmouseleave = onMouseLeaveHandler;
-    container.onmousemove = onMouseMoveHandler;
-  })();
+//     container.onmouseenter = onMouseEnterHandler;
+//     container.onmouseleave = onMouseLeaveHandler;
+//     container.onmousemove = onMouseMoveHandler;
+//   })();
   
